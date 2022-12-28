bae688a9aac10e32c2c44eaab62ef538b53b8391
RCP Rajapaksha <it20137250@my.sliit.lk>
2022-12-28 13:06:41 +0530

    Change app name

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
                 textAlign="center"
             >
                 <Typography fontWeight="bold" fontSize="32px" color="primary">
-                    Sociopedia
+                    RCP SpotChat
                 </Typography>
             </Box>
 
@@ -25,7 +25,7 @@ const LoginPage = () => {
                 backgroundColor={theme.palette.background.alt}
             >
                 <Typography textAlign="center" fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-                    Welcome to Socipedia, the Social Media for Sociopaths!
+                    Welcome to RCP SpotChat, the Social Media for Sociopaths!
                 </Typography>
                 <Form />
             </Box>
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
                         },
                     }}
                 >
-                    Sociopedia
+                    RCP SpotChat
                 </Typography>
                 {isNonMobileScreens && (
                     <FlexBetween
